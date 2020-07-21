0edced9d06e11012f5bfcb9587bbe1e305454c3f
mmhernandez <mauriciohdez08@gmail.com>
2020-07-20 21:00:13 -0500

    Logic finished

--- src/main.py
+++ src/main.py
@@ -1,18 +1,33 @@
 from flask import Flask, request, jsonify, redirect
-from utils import new_slug
+from utils import new_slug, validate_slug
+
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient()
+
+# url_shortener is the name of my db
+db = client.url_shortener
+
+# urls is the collection name
+urls = db.urls
+
 @app.route('/')
 def index():
+    # print urls.find_one({"slug":"mau"})
     return 'URL Shortener... In Python! ... But Python 2.7 :('
 
-@app.route('/<id>', methods=['GET'])
-def reroute(id):
-    # TODO: Get the actual URL from the DB given the id
-    url = 'https://google.com' if id != 'mau' else 'https://mau-frbs.dev'
+@app.route('/<slug>', methods=['GET'])
+def reroute(slug):
 
-    # TODO: Redirect to saved URL
+    query = urls.find_one({"slug": slug.lower()})
+
+    if not query:
+        return "Slug not found"
+
+    url = query.get("url")
+
     return redirect(url, code=302)
 
 @app.route('/url', methods=['POST'])
@@ -24,17 +39,32 @@ def add_url():
 
     # NOTE: What would happen if someone tries to save a slug to redirect to
     # the shortener URL ?
-    # We'll use localhost in this example
 
-    if 'short.ener' in url:
-        # TODO: Fix ugly exception (maybe?)
-        raise Exception('Don\'t')
+    # if 'short.ener' in url:
+    #     # TODO: Fix ugly exception (maybe?)
+    #     raise Exception('Don\'t')
 
-    # TODO: validate slug or create one in case no slug was provided
+    # Validate slug has only ascii characters
+    valid = validate_slug(slug)
 
-    # TODO: check whether the slug is already in use
+    if not valid:
+        return jsonify({"status": "Failure",
+                        "message": "Slug contains not only ascii characters"})
 
-    # TODO: Add slug and URL to DB
+    # Check whether the slug is already in use
+    query = urls.find_one({"slug": slug.lower()})
+
+    if query:
+        return jsonify({"status": "Failure",
+                        "message": "Slug already in use"})
+
+    # Add slug and URL to DB
+    doc = {
+        "url": url,
+        "slug": slug.lower()
+    }
+
+    urls.insert_one(doc)
 
     return jsonify({
         'status': 'Success',
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,11 @@ import random, string
 # default is length 5
 def new_slug(length=5):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+# Validates the slug. Only ascii characters allowed
+def validate_slug(slug):
+    try:
+        slug.decode('ascii')
+    except (UnicodeDecodeError, UnicodeEncodeError):
+        return False
+    return True
